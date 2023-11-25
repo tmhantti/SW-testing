bd3ace7ac4a1f6e0c17a407a18a7fa4cb66733a3
Tatu Anttila <tatu.anttila@tuni.fi>
2023-11-25 17:43:12 +0200

    tests for words.js added

---
+++ sw-test-app/tests/words.test.js
@@ -0,0 +1,29 @@
+import words from '../src/util_library/words';
+const expect = require('chai').expect;
+
+describe('words', () => {
+  it('should split string into words using default pattern', () => {
+    const result = words('fred, barney, & pebbles');
+    expect(result).to.deep.equal(['fred', 'barney', 'pebbles']);
+  });
+
+  it('should split string into words using provided pattern', () => {
+    const result = words('fred, barney, & pebbles', /[^, ]+/g);
+    expect(result).to.deep.equal(['fred', 'barney', '&', 'pebbles']);
+  });
+
+  it('should handle empty string', () => {
+    const result = words('');
+    expect(result).to.deep.equal([]);
+  });
+
+  it('should handle string with Unicode words', () => {
+    const result = words('你好, 世界');
+    expect(result).to.deep.equal(['你好', '世界']);
+  });
+
+  it('should handle string with mixed Unicode and ASCII words', () => {
+    const result = words('Hello, 你好, World');
+    expect(result).to.deep.equal(['Hello', '你好', 'World']);
+  });
+});
